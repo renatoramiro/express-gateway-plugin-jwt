805b19dc75756183075127692900a40c65f4d0f8
Renato Ramiro <renato.ramiro@virtus.ufcg.edu.br>
2017-10-24 11:57:14 -0300

    Removing condition from manifest.js

--- manifest.js
+++ manifest.js
@@ -2,7 +2,6 @@ module.exports = {
   version: '1.0.0',
   init: function (pluginContext) {
     pluginContext.registerPolicy(require('./policies/example-policy'));
-    pluginContext.registerCondition(require('./policies/condition'));
   },
   policies:['test'], // this is for CLI to automatically add to "policies" whitelist in gateway.config
   options: {  // This is for CLI to ask about params 'eg plugin configure example'
