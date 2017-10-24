6f76ece3652dbcd39708991c74fe5d535f95bccb
Renato Ramiro <renato.ramiro@virtus.ufcg.edu.br>
2017-10-24 11:33:03 -0300

    Improving police

--- policies/condition.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'token',
-  handler: function (req, conditionConfig) {
-    console.log('condition', req.query);
-    return false;
-  }
-};
--- policies/example-policy.js
+++ policies/example-policy.js
@@ -1,7 +1,21 @@
+var jwt = require('jsonwebtoken');
+
 module.exports = {
   name: 'test',
   policy: (actionParams) => {
     return (req, res, next) => {
+      var token = req.query.token || req.headers['x-access-token'];
+      if (token) {
+        jwt.verify(token, actionParams.secret, function(err, decoded) {
+          if (err) return res.json({message: err.message}, 403).end();
+          else {
+            req.decoded = decoded;
+            next();
+          }
+        });
+      } else {
+        return res.json({message: 'No token provided'}, 403);
+      }
       console.log('policy', req.query);
       next();
     };
