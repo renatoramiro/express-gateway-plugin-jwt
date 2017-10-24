93849e5d6925f0babfe116d75d446109f21ca107
Renato Ramiro <renato.ipj@gmail.com>
2017-10-23 22:20:13 -0300

    Adding a condition

--- conditions/url-match.js
+++ conditions/url-match.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: 'url-match',
   handler: function (req, conditionConfig) {
+    console.log(req.params);
     return (conditionConfig.expected === req.url);
   }
 };
--- manifest.js
+++ manifest.js
@@ -2,6 +2,7 @@ module.exports = {
   version: '1.0.0',
   init: function (pluginContext) {
     pluginContext.registerPolicy(require('./policies/example-policy'));
+    pluginContext.registerCondition(require('./conditions/url-match'));
   },
   policies:['test'], // this is for CLI to automatically add to "policies" whitelist in gateway.config
   options: {  // This is for CLI to ask about params 'eg plugin configure example'
